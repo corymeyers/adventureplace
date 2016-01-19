5cc90be01472c08d3c3b81c080e97d864eee9db8
cory meyers <whippoorwill.in.flight@gmail.com>
2016-01-18 20:03:18 -0800

    add name field fetch

---
+++ yourvacation/js/yourvacation.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  $('#allform form').submit(function(event) {
+    var personal1Input = $("input#personal1").val();
+    $('.personal1').text(personal1Input);
+    $('#vacation1show').show();
+    $('#vacation2show').show();
+    $('#vacation3show').show();
+    $('#vacation4show').show();
+
+      event.preventDefault();
+    });
+
+  $(".clickable1").click(function() {
+      $("#vacation1show").toggle();
+      $("#vacation1hide").toggle();
+    });
+
+  $(".clickable2").click(function() {
+      $("#vacation2show").toggle();
+      $("#vacation2hide").toggle();
+    });
+
+  $(".clickable3").click(function() {
+      $("#vacation3show").toggle();
+      $("#vacation3hide").toggle();
+    });
+
+
+});
