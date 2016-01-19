4386f86e6ff27f45b253f4e4fce4aeeea8cf6ead
cory meyers <whippoorwill.in.flight@gmail.com>
2016-01-18 21:02:02 -0800

    add inital form fetch

--- yourvacation/js/yourvacation.js
+++ yourvacation/js/yourvacation.js
@@ -2,18 +2,26 @@ $(document).ready(function(){
 
   $('form').submit(function(event) {
     var personal1Input = $("input#personal1").val();
+    var q1Input = $("input#question1").val();
+    var q2Input = $("input#question2").val();
+    var q3Input = $("input#question3").val();
+    var q4Input = $("input#question4").val();
+    var q5Input = $("input#question5").val();
+    var score = ["q1Input" + "q2Input" + "q3Input" + "q4Input" + "q5Input"];
 
-
-
-
-
+  
 
     $('.personal1').text(personal1Input);
-    $('#vacation1show').show();
-    $('#vacation2show').show();
-    $('#vacation3show').show();
-    $('#vacation4show').show();
 
+    if(score = 1a2a3a4a5a) {
+      $('#vacation1show').show();
+    } else if (score = 1b2a3a4a5a) {
+        $('#vacation2show').show();
+    } else if (score = 1a2b3a4a5a) {
+        $('#vacation3show').show();
+    } else {
+    $('#vacation4show').show();
+    }
 
       event.preventDefault();
     });
